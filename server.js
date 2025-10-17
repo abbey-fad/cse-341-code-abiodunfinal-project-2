270767e44625a305c0c9e363f1f02839894f4366
abbey-fad <adeabbey212@gmail.com>
2025-10-17 17:34:03 +0100

    port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
